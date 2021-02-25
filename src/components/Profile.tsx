4717479e092a3cbbdf30a8c6d63130c12fa21801
NetoJL <zeneto2604@gmail.com>
2021-02-25 11:05:27 -0300

    Feature: Profile component finished

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
 import styles from "../styles/components/Profile.module.css";
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/NetoJL.png" alt="José Neto" />
@@ -8,7 +12,7 @@ export function Profile() {
         <strong>José Neto</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
